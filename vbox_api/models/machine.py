89a13cceb3fff06573ce11903d0ffebb7e42634d
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-04-22 20:54:25 +0100

    Add saved_screenshot property to Machine model

--- vbox_api/models/machine.py
+++ vbox_api/models/machine.py
@@ -1,4 +1,8 @@
-from typing import Literal
+import base64
+import io
+from typing import Literal, Optional
+
+from PIL import Image
 
 from vbox_api.api.handle import Handle
 from vbox_api.models.base import BaseModel
@@ -18,3 +22,14 @@ class Machine(BaseModel):
             self.lock_machine(session_handle, lock_type)
             locked_machine = self.ctx.session.get_machine()
         return Machine(self.ctx, Handle(self.ctx, locked_machine))
+
+    @property
+    def saved_screenshot(self) -> Optional[Image.Image]:
+        """Return screenshot of saved state if available."""
+        try:
+            info = self.query_saved_screenshot_info(0)
+            image_dict = self.read_saved_screenshot_to_array(0, info["returnval"][0])
+            image = Image.open(io.BytesIO(base64.b64decode(image_dict["returnval"])))
+        except Exception:
+            image = None
+        return image
