d1ed7caef4356b63173e431fd1a1f34c9875b2aa
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-04-23 22:00:30 +0100

    Add thumbnail support and add cards to HTTP dashboard

--- vbox_api/models/machine.py
+++ vbox_api/models/machine.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 from vbox_api.api.handle import Handle
 from vbox_api.models.base import BaseModel
+from vbox_api.utils import image_to_data_uri, text_to_image
 
 
 class Machine(BaseModel):
@@ -23,13 +24,25 @@ class Machine(BaseModel):
             locked_machine = self.ctx.session.get_machine()
         return Machine(self.ctx, Handle(self.ctx, locked_machine))
 
-    @property
-    def saved_screenshot(self) -> Optional[Image.Image]:
+    def get_thumbnail(self, data_uri: bool = False) -> Image.Image | str:
+        """
+        Return thumbnail for instance of machine.
+
+        If data_uri is True, return base64-encoded data URI.
+        """
+        image = self.get_saved_screenshot()
+        if not image:
+            image = text_to_image(self.name)
+        if data_uri:
+            image = image_to_data_uri(image)
+        return image
+
+    def get_saved_screenshot(self) -> Optional[Image.Image]:
         """Return screenshot of saved state if available."""
         try:
             info = self.query_saved_screenshot_info(0)
             image_dict = self.read_saved_screenshot_to_array(0, info["returnval"][0])
             image = Image.open(io.BytesIO(base64.b64decode(image_dict["returnval"])))
         except Exception:
-            image = None
+            return None
         return image
---
+++ vbox_api/utils.py
@@ -0,0 +1,34 @@
+import base64
+from io import BytesIO
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def image_to_data_uri(image: Image.Image) -> str:
+    """Return data URI of image."""
+    buffer = BytesIO()
+    image_format = image.format if image.format else "PNG"
+    image.save(buffer, format=image_format)
+    image_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return f"data:image/{image_format};base64,{image_b64}"
+
+
+def text_to_image(
+    text: str,
+    size: tuple[int, int] = (800, 600),
+    bg_colour: str = "black",
+    font: ImageFont.ImageFont | ImageFont.FreeTypeFont = ImageFont.load_default(48),
+    font_colour: str = "white",
+) -> Image.Image:
+    """Return image with specified text."""
+    width, height = size
+    image = Image.new("RGB", size, bg_colour)
+    draw = ImageDraw.Draw(image)
+    _, _, bbox_width, bbox_height = draw.textbbox((0, 0), text, font=font)
+    draw.text(
+        ((width - bbox_width) / 2, (height - bbox_height) / 2),
+        text,
+        font=font,
+        fill=font_colour,
+    )
+    return image
