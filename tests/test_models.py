b71c8b0b606bae5470220af4218b4b21642003dd
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-05-14 21:08:26 +0100

    Add test for BaseModelRegister

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,8 @@
 def test_machine_get_attribute(random_machine: "Machine") -> None:
     """Test getting a property as an attribute."""
     assert random_machine.name == random_machine.get_name()
+
+
+def test_base_model_register_handles(api: "VBoxAPI") -> None:
+    """Test getting multiple instances of model returns same object."""
+    assert api.get_machines()[0] is api.get_machines()[0]
