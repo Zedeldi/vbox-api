e31a5feda70851678758c71fb399f3355f2d8e7e
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-05-16 17:15:46 +0100

    Compile Handle.HANDLE_REGEX for performance

--- vbox_api/api/handle.py
+++ vbox_api/api/handle.py
@@ -4,7 +4,7 @@ import re
 class Handle(str):
     """Class to store and provide methods to release a handle."""
 
-    HANDLE_REGEX = "[0-9a-fA-F]{16}-[0-9a-fA-F]{16}"
+    HANDLE_REGEX = re.compile("[0-9a-fA-F]{16}-[0-9a-fA-F]{16}")
 
     def __new__(cls, ctx: "Context", handle: str) -> "Handle":
         """Return new string object."""
