8e488aa5625667f893a9a3450c65ca4562211b0e
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-05-20 19:34:44 +0100

    Allow URL parameter name to be specified explicitly for model conversion

--- vbox_api/http/utils.py
+++ vbox_api/http/utils.py
@@ -7,10 +7,10 @@ from flask import abort, g, request
 
 
 def get_model_from_name_or_id(
-    model_name: str, name_or_id: Optional[str]
+    model_name: str, name_or_id: Optional[str] = None, url_parameter: str = "id"
 ) -> "BaseModel":
     """Return model instance from name_or_id for model_name or abort request."""
-    name_or_id = name_or_id or request.args.get("id")
+    name_or_id = name_or_id or request.args.get(url_parameter)
     if not name_or_id:
         abort(400, f"No {model_name.lower()} specified.")
     model = g.api.find_model(model_name, name_or_id)
@@ -19,13 +19,13 @@ def get_model_from_name_or_id(
     return model
 
 
-def convert_id_to_model(model_name: str) -> Callable:
+def convert_id_to_model(model_name: str, url_parameter: str = "id") -> Callable:
     """Decorate a function to convert model ID or name to object."""
 
     def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
         def inner(name_or_id: Optional[str] = None, *args, **kwargs) -> Any:
-            model = get_model_from_name_or_id(model_name, name_or_id)
+            model = get_model_from_name_or_id(model_name, name_or_id, url_parameter)
             return func(model, *args, **kwargs)
 
         return inner
