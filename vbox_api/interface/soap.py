6bc7daa0e7dcfd0240c11564a99749077f1d623a
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-05-25 13:08:12 +0100

    Add method to SOAPInterface to get enums from WSDL

--- vbox_api/interface/soap.py
+++ vbox_api/interface/soap.py
@@ -1,8 +1,11 @@
 """SOAP interface implementation to VirtualBox API."""
 
+from pathlib import Path
 from typing import Optional
 
+import requests
 import zeep
+from lxml import etree
 
 from vbox_api.interface.base import BaseInterface, ProxyInterface
 
@@ -11,6 +14,11 @@ class SOAPInterface(BaseInterface):
     """Define interface class for SOAP methods."""
 
     BINDING_QNAME = "{http://www.virtualbox.org/}vboxBinding"
+    TYPES_QNAME = "{http://schemas.xmlsoap.org/wsdl/}types"
+    SCHEMA_QNAME = "{http://www.w3.org/2001/XMLSchema}schema"
+    SIMPLETYPE_QNAME = "{http://www.w3.org/2001/XMLSchema}simpleType"
+    RESTRICTION_QNAME = "{http://www.w3.org/2001/XMLSchema}restriction"
+    ENUMERATION_QNAME = "{http://www.w3.org/2001/XMLSchema}enumeration"
 
     def __init__(self, host: str = "localhost", port: int = 18083) -> None:
         """Initialise instance of interface."""
@@ -37,3 +45,23 @@ class SOAPInterface(BaseInterface):
                 proxy_interface = ProxyInterface()
                 self._register_interface(interface_name, proxy_interface)
             proxy_interface._register_method(method_name, method_callable)
+
+    def get_enums(self, path: Optional[str | Path] = None) -> dict[str, list[str]]:
+        """Return dictionary of enums in WSDL document."""
+        if path:
+            with open(path) as fd:
+                wsdl = etree.parse(fd)
+            root = wsdl.getroot()
+        else:
+            wsdl = requests.get(self.wsdl).content
+            root = etree.fromstring(wsdl)
+        schema = root.find(self.TYPES_QNAME).find(self.SCHEMA_QNAME)
+        return {
+            element.get("name"): [
+                enum.get("value")
+                for enum in element.find(self.RESTRICTION_QNAME).findall(
+                    self.ENUMERATION_QNAME
+                )
+            ]
+            for element in schema.findall(self.SIMPLETYPE_QNAME)
+        }
