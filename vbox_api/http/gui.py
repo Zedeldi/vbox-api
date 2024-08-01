e3c122b27bb7ff2812a6adc15ca07be91f8d2c1c
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2024-08-01 14:12:13 +0100

    Add entry-point for vbox_api.http.gui

--- vbox_api/http/gui.py
+++ vbox_api/http/gui.py
@@ -6,5 +6,11 @@ from vbox_api.http import app
 
 ui = FlaskUI(app=app, server="flask")
 
-if __name__ == "__main__":
+
+def main() -> None:
+    """Start FlaskUI desktop GUI application."""
     ui.run()
+
+
+if __name__ == "__main__":
+    main()
